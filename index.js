3b4525791bf9474198d3af714fde66ce3878966f
Piotr Bender <piotr.bender@silevis.com>
2022-03-18 21:06:59 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const {faker} = require('@faker-js/faker');
+module.exports = () => {
+  const { faker } = require('@faker-js/faker');
+
+  const CATEGORIES_COUNT = 10;
+  const ADVERTS_COUNT = 100;
+
+  const data = { }
+  data.categories = Array.from(Array(CATEGORIES_COUNT).keys()).map((i) => ({
+    id: i,
+    title: faker.commerce.department(),
+  }));
+  data.adverts = Array.from(Array(ADVERTS_COUNT).keys()).map((i) => ({
+    id: i,
+    title: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    description: faker.commerce.productDescription(),
+    seller: faker.name.firstName(),
+    image: faker.image.business(400, 400),
+    sellerPhone: faker.phone.phoneNumber('+48 ### ### ###'),
+    canNegotiate: faker.datatype.boolean(),
+    createdOn: faker.date.recent(),
+    categoryId: faker.datatype.number(CATEGORIES_COUNT - 1)
+  }));
+  return data
+}
