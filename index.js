e5516b44fb1fe77204bb11d8bb49e4d7836b4898
Piotr Bender <piotr.bender@silevis.com>
2022-05-08 18:55:50 +0200

    requests update

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const {faker} = require('@faker-js/faker');
 module.exports = () => {
-  const { faker } = require('@faker-js/faker');
+  const {faker} = require('@faker-js/faker');
 
   const CATEGORIES_COUNT = 10;
-  const ADVERTS_COUNT = 100;
+  const ADVERTS_COUNT = 30;
 
-  const data = { }
-  data.categories = Array.from(Array(CATEGORIES_COUNT).keys()).map((i) => ({
-    id: i,
-    title: faker.commerce.department(),
-  }));
-  data.adverts = Array.from(Array(ADVERTS_COUNT).keys()).map((i) => ({
-    id: i,
-    title: faker.commerce.productName(),
-    price: faker.commerce.price(),
-    description: faker.commerce.productDescription(),
-    seller: faker.name.firstName(),
-    image: `${faker.image.business(400, 400)}?i=${i}`,
-    sellerPhone: faker.phone.phoneNumber('+48 ### ### ###'),
-    canNegotiate: faker.datatype.boolean(),
-    createdOn: faker.date.recent(),
-    categoryId: faker.datatype.number(CATEGORIES_COUNT - 1)
-  }));
-  return data
+  return ({
+    name: {
+      name: faker.name.firstName(),
+      avatar: faker.image.avatar()
+    },
+    categories: Array.from(Array(CATEGORIES_COUNT).keys()).map((i) => ({
+      id: i,
+      title: faker.commerce.department(),
+    })),
+    adverts: Array.from(Array(ADVERTS_COUNT).keys()).map((i) => ({
+      id: i,
+      title: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      description: faker.commerce.productDescription(),
+      seller: faker.name.firstName(),
+      image: `${faker.image.business(400, 400)}?i=${i}`,
+      sellerPhone: faker.phone.phoneNumber('+48 ### ### ###'),
+      canNegotiate: faker.datatype.boolean(),
+      createdOn: faker.date.recent(),
+      categoryId: faker.datatype.number(CATEGORIES_COUNT - 1)
+    }))
+  })
 }
